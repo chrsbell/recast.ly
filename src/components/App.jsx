dfded6f1b46e60ab129635f70f7ff68a76a8422b
Chris Bell <cb.camellia@gmail.com>
2020-09-01 11:25:31 -0700

    Refactor stateless functional App to ES6 class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,62 @@
 import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
 
-var App = (props) => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // set the state with a currentVideo property, the value set to empty for now until cliked, then add video to player
+    // set this.state to equal the videos title
+    console.log(props);
+    this.state = {currentVideosTitle: props.videos[0].snippet.title, videoList: props.videos};
+    // this.state = {videoList: props};
+  }
+
+
+  // make an onClick method
+  onVideoTitleClick(event) {
+    console.log(event);
+    debugger;
+    // event.target.textContent gives title
+    console.log('Title Clicked');
+    this.setState({
+      currentVideosTitle: event.target.textContent
+    });
+  }
+  // when clicked, adds the video cliked to the VideoPLayer
+
+  render() {
+
+    // create a object
+    // change currentVideo to the one that is clicked
+
+    var currentVideo = this.state.videoList[0];
+
+    for (var i = 1; i < this.state.videoList.length; i++) {
+      console.log('VideoListEntry #' + i + this.state.videoList[i].snippet.title);
+      console.log('Video Title Clicked ' + this.state.currentVideosTitle);
+      // check if each title in video list matches the title that was clicked
+      if (this.state.videoList[i].snippet.title === this.state.currentVideosTitle) {
+        currentVideo = this.state.videoList[i];
+      }
+    }
+
+    return (<div>
+      <nav className="navbar">
+        <div className="col-md-6 offset-md-3">
+          <div><h5><em>search</em> view goes here</h5></div>
+        </div>
+      </nav>
+      <div className="row">
+        <div className="col-md-7">
+          <div><h5><em>videoPlayer</em><VideoPlayer video={currentVideo} /></h5></div>
+        </div>
+        <div className="col-md-5">
+          <div onClick={this.onVideoTitleClick.bind(this)} ><h5><em>videoList</em><VideoList videos={this.state.videoList} /></h5></div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em><VideoList videos={props.data} /></h5></div>
-      </div>
-    </div>
-  </div>
-);
+    </div>);
+  }
+}
 
 
 
